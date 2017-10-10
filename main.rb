c39b9c9bb08b57d184145dcee4d609fa27694d40
Alex Ianus <hire@alexianus.com>
2017-10-10 15:23:41 -0400

    Remove the loop and act only on a single currency

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@ require 'pry'
 require 'bitfinex'
 require 'bigdecimal'
 
+CURRENCY = (ENV['CURRENCY'] || 'usd').downcase
 OFFER_DURATION = (ENV['OFFER_DURATION'] || '2').to_i
 POLLING_INTERVAL = (ENV['POLLING_INTERVAL'] || '120').to_i
 
@@ -15,45 +16,41 @@ Bitfinex::Client.configure do |conf|
 end
 client = Bitfinex::Client.new
 
-loop do
-  # Cancel unfilled offers
-  client.offers.each do |offer|
-    client.cancel_offer(offer['id'])
+# Cancel unfilled offers
+client.offers.each do |offer|
+  client.cancel_offer(offer['id'])
+end
+
+# Wait for eventual consistency
+sleep 2
+
+available_deposit_balances = Hash[*
+  client
+    .balances
+    .select{|b| b['type'] == 'deposit'}
+    .flat_map{|b| [b['currency'], b['available']]}
+]
+
+available_deposit_balance = available_deposit_balances[CURRENCY]
+
+puts "Available balance of #{CURRENCY.upcase}#{available_deposit_balance} found"
+
+# Get the edge of the order book
+best_ask = BigDecimal.new(client.funding_book(CURRENCY, limit_bids: 1, limit_asks: 1)['asks'][0]['rate'])
+our_ask  = best_ask - BigDecimal('0.1')
+# Place an offer for all of our money
+begin
+  client.new_offer(
+    CURRENCY,
+    available_deposit_balance,
+    our_ask.to_s('F'),
+    OFFER_DURATION,
+    'lend'
+  )
+  puts "Placed offer for #{CURRENCY.upcase}#{available_deposit_balance} at #{our_ask.to_s('F')}% per year"
+rescue Bitfinex::BadRequestError => e
+  # Super janky but it's not worth it to figure this out ourselves
+  if !(e.message.include?('minimum is 50 dollar') || e.message.include?('must be positive'))
+    puts e
   end
-
-  # Wait for eventual consistency
-  sleep 2
-
-  available_deposit_balances = Hash[*
-    client
-      .balances
-      .select{|b| b['type'] == 'deposit'}
-      .flat_map{|b| [b['currency'], b['available']]}
-  ]
-
-  available_deposit_balances.each do |currency, amount|
-    next if currency == 'eth'
-
-    # Get the edge of the order book
-    best_ask = BigDecimal.new(client.funding_book(currency, limit_bids: 1, limit_asks: 1)['asks'][0]['rate'])
-    our_ask  = best_ask - BigDecimal('0.1')
-    # Place an offer for all of our money
-    begin
-      client.new_offer(
-        currency,
-        amount,
-        our_ask.to_s('F'),
-        OFFER_DURATION,
-        'lend'
-      )
-      puts "Placed offer for #{currency} #{amount} at #{our_ask.to_s('F')}% per year"
-    rescue Bitfinex::BadRequestError => e
-      # Super janky but it's not worth it to figure this out ourselves
-      if !(e.message.include?('minimum is 50 dollar') || e.message.include?('must be positive'))
-        puts e
-      end
-    end
-  end
-
-  sleep POLLING_INTERVAL
 end
